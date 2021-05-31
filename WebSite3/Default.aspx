2feb9768dd6c425a5989bb468e2d7c6e2f835872
Halef BUDANUR <62338386+hbdnr@users.noreply.github.com>
2021-05-31 13:40:26 +0300

    Add files via upload

---
+++ WebSite3/Default.aspx
@@ -0,0 +1,30 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeFile="Default.aspx.cs" Inherits="_Default" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+    <div>
+    
+        Dosya Seçin:&nbsp;&nbsp;&nbsp;
+        <asp:FileUpload ID="FileUpload1" runat="server" />
+        <br />
+        <br />
+        <asp:Button ID="Button1" runat="server" OnClick="Button1_Click" Text="Kontrol et" />
+&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; Sonuç:&nbsp;&nbsp;&nbsp;
+        <asp:Label ID="Label1" runat="server"></asp:Label>
+&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+        <br />
+        <br />
+        <asp:Button ID="Button2" runat="server" OnClick="Button2_Click" Text="Boyut" />
+&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; Boyut:&nbsp;&nbsp;&nbsp;
+        <asp:Label ID="Label2" runat="server"></asp:Label>
+    
+    </div>
+    </form>
+</body>
+</html>
---
+++ WebSite3/Default.aspx.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+public partial class _Default : System.Web.UI.Page
+{
+    public string yol = @"C:\Users\Yusuf\Desktop\Dersler 3-2\Web tasarımı ve programlama\odevler\hafta 10\WebSite3\veri";
+    protected void Page_Load(object sender, EventArgs e)
+    {
+
+    }
+    protected void Button1_Click(object sender, EventArgs e)
+    {
+        if (FileUpload1.HasFile)
+        {
+            if (Path.GetExtension(yol + "\\" + FileUpload1.FileName) == ".pdf" || Path.GetExtension(yol + "\\" + FileUpload1.FileName) == ".docx")
+            {
+                FileUpload1.SaveAs(Server.MapPath("veri") + "\\" + FileUpload1.FileName);
+                Label1.Text = "Eklendi...";
+            }else
+            {
+                Response.Write("Boş Girdiniz veya Dosya türü hatalıdır...");
+
+            }
+
+        }else
+        {
+            Response.Write("Boş Girdiniz veya Dosya türü hatalıdır...");
+
+        }
+    }
+
+    protected void Button2_Click(object sender, EventArgs e)
+    {
+        long toplam = 0;
+        string[] gelen = Directory.GetFiles(yol, "*.*", SearchOption.AllDirectories);
+        foreach (string k in gelen)
+        {
+            FileInfo aa = new FileInfo(k);
+            toplam += aa.Length;
+        }
+
+        Label2.Text = "Bayt: " + toplam.ToString() + " --- KiloBayt: " + ((double)toplam / (double)1024).ToString() + " --- MegaBayt: " + ((double)toplam / (double)1048576).ToString() + " --- GigaBayt: " + ((double)toplam / (double)1073741824).ToString();
+    }
+}
